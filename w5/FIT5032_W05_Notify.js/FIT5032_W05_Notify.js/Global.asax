e1203d0540427c2ece0c7d17997d37f7931ede75
Farhad Ullah Rezwan <frez0003@student.monash.edu>
2019-08-30 10:40:45 +1000

    Week 5

---
+++ w5/FIT5032_W05_Notify.js/FIT5032_W05_Notify.js/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_W05_Notify.js.MvcApplication" Language="C#" %>
---
+++ w5/Week5_DatePicker/Week5_DatePicker/Models/Date.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week5_DatePicker.Models
+{
+    public class Date
+    {
+        public int ID { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
