da52d4c97c2d3d82e695e65a8ff32925f89a5d2d
Farhad34 <frez0003@student.monash.edu>
2019-10-12 19:07:19 +1100

    Lab Assessment 3 Bulk Email

---
+++ Assessment 3 - 2%/EmailWAttachment/EmailWithAttachment/EmailWithAttachment/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmailWithAttachment.Models;
+using EmailWithAttachment.Utilities;
+
+namespace EmailWithAttachment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SendEmailWithAttachment()
+        {
+            return View(new EmailWithAttachmentViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult SendEmailWithAttachment(EmailWithAttachmentViewModel model)
+        {
+            try
+            {
+                string path = Server.MapPath("~/Uploads");
+                string fileName = Path.GetFileName(model.Attachment.FileName);
+                string fullPath = Path.Combine(path, fileName);
+                model.Attachment.SaveAs(fullPath);
+
+                EmailWithAttachmentSender emailSender = new EmailWithAttachmentSender();
+                emailSender.Sender(model, fullPath);
+
+                ViewBag.Result = "Success: E-mail sent";
+
+                ModelState.Clear();
+
+                return View(new EmailWithAttachmentViewModel());
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assessment 3 - 2%/EmailWAttachment/EmailWithAttachment/EmailWithAttachment/Models/EmailWithAttachmentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmailWithAttachment.Models
+{
+    public class EmailWithAttachmentViewModel
+    {
+        [Display(Name = "E-mail address")]
+        [Required(ErrorMessage = "Please enter an e-mail address")]
+        [EmailAddress(ErrorMessage = "Enter valid e-mail address")]
+
+        public string EmailTo { get; set; }
+
+        [Required(ErrorMessage = "Please provide a subject")]
+        public string Sub { get; set; }
+
+        [Required(ErrorMessage = "Please enter content")]
+
+        public string EmailContent { get; set; } 
+
+        public HttpPostedFileBase Attachment { get; set; }
+
+    }
+}
